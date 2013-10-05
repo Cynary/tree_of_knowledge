b4c3bbfbab4ef7309e8a940a3f3e9c054146f104
Lactor <machado007@gmail.com>
2013-10-05 16:32:27 -0400

    Debug started

---
+++ debug.py
@@ -0,0 +1,21 @@
+from node import node
+Tree = {}
+
+def findNode(name = ""):
+    for i in Tree.values():
+        if i.name == name:
+            return i
+
+Tree[node.count] = node("Root", set(["Root", "First step"]), 'First node')
+
+print "ROOT ADDED"
+for i in Tree.values():
+    print i.name, i.keywords, i.parents, i.children
+
+Tree[node.count] = node("child", set(["child"]), 'Child', ([findNode("Root")]))
+
+print "LEAF added"
+for i in Tree.values():
+    print i.name, i.keywords, i.parents, i.children
+
+print Tree
--- node.py
+++ node.py
@@ -1,13 +1,20 @@
 class node:
     count = 0
-    def __init__(self, name, keywords = set([]), content = '', parents = set([]), children = set([])):
-        self.parents = parents
-        self.children = children
+    def __init__(self, name, keywords = set([]), content = '', parents = {}, children = {}):
+
+        self.ID = self.count
+        node.count = node.count+1
+
+        self.parents = {}
+        for parent in parents:
+            self.addParent(parent)
+        self.children = {}
+        for child in children:
+            self.addChild(child)
         self.content = content
         self.keywords = keywords
         self.name = name
-        self.ID = self.count
-        node.count = node.count+1
+       
 
     def getName(self):
         return self.name
@@ -19,13 +26,13 @@ class node:
         return self.children
 
     def addParent(self, parent):
-        if parent not in self.parents:
-            self.parents.add(parent)
+        if parent.ID not in self.parents:
+            self.parents[parent.ID] = parent
             parent.addChild(self)
 
     def addChild(self, child):
-        if child not in self.children:
-            self.children.add(child)
+        if child.ID not in self.children:
+            self.children[child.ID] = child
             child.addParent(self)
 
     def getContent(self):
@@ -41,13 +48,13 @@ class node:
         self.content = newContent
 
     def removeParent(self, oldParent):
-        if oldParent in self.parents:
-            self.parents.remove(oldParent)
+        if oldParent.ID in self.parents:
+            del self.parents[oldParent.ID]
             oldParent.removeChild(self)
 
     def removeChild(self, oldChild):
-        if oldChild in self.children:
-            self.children.remove(oldChild)
+        if oldChild.ID in self.children:
+            del self.children[oldChild.ID]
             oldChild.removeParent(self)
 
     def __hash__(self):
